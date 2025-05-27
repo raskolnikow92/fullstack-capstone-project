f6661d6ec9332e6efcddf420ed9c2515e785d717
Kai <kai.schuleit@hotmail.de>
2025-05-27 11:19:12 -0400

    implement login and register page

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -5,9 +5,14 @@ import './RegisterPage.css';
 function RegisterPage() {
 
     //insert code here to create useState hook variables for firstName, lastName, email, password
-
+    const [ firstName, setFirstName ] = useState('');
+    const [ lastName, setLastName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
     // insert code here to create handleRegister function and include console.log
-
+    const handleRegister = async()=>{
+        console.log('Register text');
+    }
          return (
             <div className="container mt-5">
                 <div className="row justify-content-center">
@@ -16,8 +21,46 @@ function RegisterPage() {
                             <h2 className="text-center mb-4 font-weight-bold">Register</h2>
 
                     {/* insert code here to create input elements for all the variables - firstName, lastName, email, password */}
-
+                    <div className="mb-4">
+                        <label htmlFor="firstName" className="form label"> FirstName</label><br/>
+                        <input
+                            id="firstName"
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter your firstName"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+                        <label htmlFor="lastName" className="form label"> LastName</label><br/>
+                        <input
+                            id="lastName"
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter your lastName"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+                        <label htmlFor="email" className="form label"> Email</label><br/>
+                        <input
+                            id="email"
+                            type="email"
+                            className="form-control"
+                            placeholder="Enter your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label htmlFor="password" className="form label"> Password</label><br/>
+                        <input
+                            id="password"
+                            type="password"
+                            className="form-control"
+                            placeholder="Enter your password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
                     {/* insert code here to create a button that performs the `handleRegister` function on click */}
+                    <button className='btn btn-primary' onClick={handleRegister}>Register</button>
                         <p className="mt-4 text-center">
                             Already a member? <a href="/app/login" className="text-primary">Login</a>
                         </p>
