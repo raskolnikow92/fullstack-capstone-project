4f9df83865aaf9fd3e88be77ed3b1ee947299027
Kai <kai.schuleit@hotmail.de>
2025-05-31 13:56:31 -0400

    almost done

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -35,7 +35,7 @@ router.get('/', async (req, res, next) => {
 
         // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
         // {{insert code here here}}
-        await collection.find(query).toArray();
+        const gifts = await collection.find(query).toArray();
         res.json(gifts);
     } catch (e) {
         next(e);
